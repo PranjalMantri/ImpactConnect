9bfdda8222db7f11112ada41f440260a2e2c8e9b
Pranjal Mantri <pranjalmantri@gmail.com>
2025-10-10 20:35:11 +0530

    cleanup: remove unused files

--- src/store/userStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-const useUserStore = create((set) => ({
-  isUserLoggedIn: true,
-  login: () => set({ isUserLoggedIn: true }),
-  logout: () => set({ isUserLoggedIn: false }),
-}));
-
-export default useUserStore;
